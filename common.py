90ac11403cc6e16df13427fc64bd7cbb6f8e6496
czifumasa <lukasz.lenart912@gmail.com>
2020-07-04 20:07:15 +0200

    improved duplicate detections, added playlist data for exported songs

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
-import sys
-import os
 import codecs
-
+import os
+import sys
+import time
 
 # the logfile for keeping track of things
 logfile = None
@@ -28,6 +28,21 @@ def log(message, nl=True):
         logfile.write(message)
 
 
+def create_dir_if_not_exist(path_to_dir):
+    if not os.path.exists(path_to_dir):
+        os.makedirs(path_to_dir)
+
+
+def throw_error(message):
+    log(message)
+    time.sleep(3)
+    exit()
+
+
+def flatten_list(list_to_flatten):
+    return [item for sublist in list_to_flatten for item in sublist]
+
+
 def get_duplicated_items_from_list(seq):
     seen = set()
     seen_add = seen.add
@@ -35,4 +50,3 @@ def get_duplicated_items_from_list(seq):
     seen_twice = set(x for x in seq if x in seen or seen_add(x))
     # turn the set into a list (as requested)
     return list(seen_twice)
-
--- export_playlists_ytm.py
+++ export_playlists_ytm.py
@@ -1,26 +1,28 @@
-from ytm_api_wrapper import *
 from csv_wrapper import *
-import time
+from ytm_api_wrapper import *
 
 if len(sys.argv) < 2:
-    log('ERROR output directory is required')
-    time.sleep(3)
-    exit()
+    throw_error('ERROR output directory is required')
 
 # setup the output directory, create it if needed
 output_dir = sys.argv[1]
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+create_dir_if_not_exist(output_dir)
 
 api = open_api()
 
-playlistIds = get_my_playlist_ids(api)
 
-songs = get_songs_from_playlist(api, playlistIds[8])
+def export_duplicated_songs():
+    playlists = get_my_playlist_ids_and_names(api)
 
-duplicated_song_ids = get_duplicated_song_ids(songs)
+    export_result = []
+    for playlist in playlists:
+        songs = get_songs_from_playlist_grouped_by_id(api, playlist['id'])
+        duplicated_songs = create_list_of_duplicated_sons(songs)
+        export_result.extend(export_songs(duplicated_songs, playlist))
 
-export_result = export_songs(songs, duplicated_song_ids)
-create_csv_with_list_of_dict(sys.argv[1]+'/duplicated_songs.csv', export_result)
+    headers = ['Artists', 'Title', 'FullName', 'VideoId', 'SetVideoId', 'Playlist', 'PlaylistId']
+    create_csv_with_list_of_dict(output_dir + '/duplicated_songs.csv', headers, export_result)
 
+
+export_duplicated_songs()
 sys.exit()
