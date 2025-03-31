ef0988c5d4e44b64255e956e3861a6ea99f9ab75
czifumasa <lukasz.lenart912@gmail.com>
2025-04-01 00:10:09 +0200

    Add fields to ImportedPlaylist

--- ytmusiclibtracker/dto/ImportedPlaylist.py
+++ ytmusiclibtracker/dto/ImportedPlaylist.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from ytmusiclibtracker.dto.ImportedPlaylistItem import ImportedPlaylistItem
 
@@ -8,16 +8,25 @@ class ImportedPlaylist:
             self,
             youtube_playlist_id: str,
             title: str,
-            items: List[ImportedPlaylistItem]
+            items: List[ImportedPlaylistItem],
+            description: Optional[str],
+            year: Optional[str],
+            author: Optional[str]
 
     ):
         self.youtubePlaylistId = youtube_playlist_id
         self.title = title
         self.items = items
+        self.description = description
+        self.year = year
+        self.author = author
 
     def to_dict(self):
         return {
             "youtubePlaylistId": self.youtubePlaylistId,
             "title": self.title,
+            "description": self.description,
+            "year": self.year,
+            "author": self.author,
             "items": [item.to_dict() for item in self.items]
         }
